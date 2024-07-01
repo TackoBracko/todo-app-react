504ef1449e3fd7c2984157fcc9b2f860d4de0d21
Tatjana Oljaca <tatjanaoljaca@gmail.com>
2024-07-01 19:09:53 +0200

    css changes

--- src/components/TodoButtons.jsx
+++ src/components/TodoButtons.jsx
@@ -1,12 +1,23 @@
 export default function TodoButtons({ filterTasks, clearCompletedTasks }) {
     return (
-        <div className="todo_btns">
-            <div>
-                <button className="tags_btns one" onClick={() => filterTasks('all')}>All</button>
-                <button className="tags_btns" onClick={() => filterTasks('active')}>Active</button>
-                <button className="tags_btns" onClick={() => filterTasks('completed')}>Completed</button>
-                <button className="tags_btns" onClick={clearCompletedTasks}>Clear Completed</button>
+        <>
+            <div className="todo_btns">
+                <span>5 items left</span>
+                <ul className="filter_btns_list">
+                    <li className="filter_btn one" onClick={() => filterTasks('all')}>All</li>
+                    <li className="filter_btn" onClick={() => filterTasks('active')}>Active</li>
+                    <li className="filter_btn" onClick={() => filterTasks('completed')}>Completed</li>
+                </ul>
+                <button className="clear_all_btn" onClick={clearCompletedTasks}>Clear Completed</button>
             </div>
-        </div>
+
+            <div>
+                <ul className="filter_btns_show">
+                    <li className="filter_btn one" onClick={() => filterTasks('all')}>All</li>
+                    <li className="filter_btn" onClick={() => filterTasks('active')}>Active</li>
+                    <li className="filter_btn" onClick={() => filterTasks('completed')}>Completed</li>
+                </ul>
+            </div>
+        </>
     )
 }
