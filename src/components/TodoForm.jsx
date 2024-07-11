0d788e1384e2532e81af7ed63ca9d52fa36d0dd8
Tatjana Oljaca <tatjanaoljaca@gmail.com>
2024-07-11 16:52:53 +0200

    todoform changes

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,28 +1,32 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 export default function TodoForm({ addTodoTask }) {
-    const [inputTaskValue, setInputTaskValue] = useState('')
+    //const [inputTaskValue, setInputTaskValue] = useState('')
+    const todoTask = useRef()
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        addTodoTask(inputTaskValue)
-        setInputTaskValue('')
+        addTodoTask(todoTask.current.value)
+        todoTask.current.value = ''
+        //addTodoTask(inputTaskValue)
+        //setInputTaskValue('')
     }
 
-    const handleChange = (e) => {
-        setInputTaskValue(e.target.value)
-    }
+    //const handleChange = (e) => {
+    //    setInputTaskValue(e.target.value)
+    //}
 
     return (
         <>
             <form className="form" onSubmit={handleSubmit}>
                 <button type="submit" className="submitBtn" ></button>
                 <input
+                    ref={todoTask}
                     type="text"
-                    value={inputTaskValue}
                     className="todo_input"
-                    onChange={handleChange}
                     placeholder="Create a new todo..."
+                    //value={inputTaskValue}
+                    //onChange={handleChange}
                 />
             </form>
         </>
